37aa9dc5206a56d90f50bba1f022d8bef5114f10
Yash Gosalia <yashgosalia555@gmail.com>
2020-09-01 22:09:44 +0530

    Added Fruits Module Exercise Solution

---
+++ fruits_exercise/src/helpers.js
@@ -0,0 +1,18 @@
+function choice(items)
+{
+    let index = Math.floor(Math.random() * items.length);
+    return items[index];
+}
+
+function remove(items,item)
+{
+    for(let i = 0; i < items.length; i++)
+    {
+        if(items[i] === item)
+        {
+            return [...items.slice(0,i), ...items.slice(i+1)];
+        }
+    }
+}
+
+export {choice, remove};
