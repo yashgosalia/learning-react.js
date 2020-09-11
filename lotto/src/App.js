6a461a310e572589600d365b4c33defe92ff4b99
Yash Gosalia <yashgosalia555@gmail.com>
2020-09-11 13:20:55 +0530

    Created Lottery App

---
+++ lotto/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Ball from './Ball';
+import Lottery from './Lottery';
+
+function App() {
+  return (
+    <div className="App">
+      <Lottery />
+      <Lottery title='Mini Daily' maxNum={10} numBalls={4} />
+    </div>
+  );
+}
+
+export default App;
